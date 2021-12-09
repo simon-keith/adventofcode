e2af145ee8197d5323d046106e5ec1197fbc8cb8
Simon Keith <simon.keith@shippeo.com>
2021-12-09 01:04:58 +0100

    init day 2

---
+++ tests/aoc2021/test_day03.py
@@ -0,0 +1,15 @@
+from adventofcode.aoc2021.day03 import solve_part1, solve_part2
+from pytest import fixture
+
+
+@fixture
+def puzzle_input():
+    return []
+
+
+def test_part1(puzzle_input):
+    assert solve_part1(puzzle_input) == 0
+
+
+def test_part2(puzzle_input):
+    assert solve_part2(puzzle_input) == 0
