955f01842aaaaf7e4a6302de0d068a36b77ffb7c
Simon Keith <simon.keith@shippeo.com>
2021-12-17 22:54:05 +0100

    day 5

--- adventofcode/aoc2021/day05.py
+++ adventofcode/aoc2021/day05.py
@@ -1,14 +1,52 @@
-from typing import List
+from collections import defaultdict
+from itertools import chain
+from typing import Generator, List, Tuple
 
 from adventofcode.utils.input import read_puzzle_input
 
 
+def _parse(puzzle_input: List[str]) -> Generator[Tuple[int, int, int, int], None, None]:
+    return (
+        chain.from_iterable(((int(c) for c in p.split(",")) for p in row.split(" -> ")))
+        for row in puzzle_input
+    )
+
+
+def _sign(a: int, b: int) -> int:
+    if a == b:
+        return 0
+    return 1 if b > a else -1
+
+
+def _iter_coords(
+    segment: Tuple[int, int, int, int],
+    allow_diag: bool,
+) -> Generator[Tuple[int, int], None, None]:
+    x1, y1, x2, y2 = segment
+    dx = _sign(x1, x2)
+    dy = _sign(y1, y2)
+    if allow_diag or dx == 0 or dy == 0:
+        yield x1, y1
+        while x1 != x2 or y1 != y2:
+            x1 += dx
+            y1 += dy
+            yield x1, y1
+
+
+def _solve(puzzle_input: List[str], allow_diag: bool) -> int:
+    coords_counter = defaultdict(int)
+    for segment in _parse(puzzle_input):
+        for coords in _iter_coords(segment, allow_diag):
+            coords_counter[coords] += 1
+    return sum(c > 1 for c in coords_counter.values())
+
+
 def solve_part1(puzzle_input: List[str]) -> int:
-    return
+    return _solve(puzzle_input, False)
 
 
 def solve_part2(puzzle_input: List[str]) -> int:
-    return
+    return _solve(puzzle_input, True)
 
 
 if __name__ == "__main__":
--- tests/aoc2021/test_day05.py
+++ tests/aoc2021/test_day05.py
@@ -4,12 +4,23 @@ from pytest import fixture
 
 @fixture
 def puzzle_input():
-    return []
+    return [
+        "0,9 -> 5,9",
+        "8,0 -> 0,8",
+        "9,4 -> 3,4",
+        "2,2 -> 2,1",
+        "7,0 -> 7,4",
+        "6,4 -> 2,0",
+        "0,9 -> 2,9",
+        "3,4 -> 1,4",
+        "0,0 -> 8,8",
+        "5,5 -> 8,2",
+    ]
 
 
 def test_part1(puzzle_input):
-    assert solve_part1(puzzle_input) is None
+    assert solve_part1(puzzle_input) == 5
 
 
 def test_part2(puzzle_input):
-    assert solve_part2(puzzle_input) is None
+    assert solve_part2(puzzle_input) == 12
