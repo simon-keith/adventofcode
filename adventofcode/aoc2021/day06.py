39a113ab653a8218c410d734e72059b8df0ea9f6
Simon Keith <simon.keith@shippeo.com>
2021-12-18 02:27:53 +0100

    day 6

--- adventofcode/aoc2021/day06.py
+++ adventofcode/aoc2021/day06.py
@@ -1,14 +1,44 @@
-from typing import List
+from collections import deque
+from typing import Deque, List, Tuple
 
 from adventofcode.utils.input import read_puzzle_input
 
+_CYCLE_LENGTH = 7
+_NEW_TIMER_VALUE = 8
+
+
+def _parse(puzzle_input: List[str]) -> Tuple[int, ...]:
+    (data,) = puzzle_input
+    return tuple(int(x) for x in data.split(","))
+
+
+def _init_timer(initial_state: Tuple[int, ...]) -> Deque[int]:
+    timer = [0] * (max(_NEW_TIMER_VALUE, _CYCLE_LENGTH) + 1)
+    for s in initial_state:
+        timer[s] += 1
+    return deque(timer)
+
+
+def _inc_timer(timer: Deque[int]):
+    timer.rotate(-1)
+    timer[_CYCLE_LENGTH - 1] += timer[_NEW_TIMER_VALUE]
+
+
+def _simulate(population: Tuple[int, ...], epoch: int) -> int:
+    timer = _init_timer(population)
+    for _ in range(epoch):
+        _inc_timer(timer)
+    return sum(timer)
+
 
 def solve_part1(puzzle_input: List[str]) -> int:
-    return
+    population = _parse(puzzle_input)
+    return _simulate(population, 80)
 
 
 def solve_part2(puzzle_input: List[str]) -> int:
-    return
+    population = _parse(puzzle_input)
+    return _simulate(population, 256)
 
 
 if __name__ == "__main__":
--- tests/aoc2021/test_day06.py
+++ tests/aoc2021/test_day06.py
@@ -4,12 +4,12 @@ from pytest import fixture
 
 @fixture
 def puzzle_input():
-    return []
+    return ["3,4,3,1,2"]
 
 
 def test_part1(puzzle_input):
-    assert solve_part1(puzzle_input) is None
+    assert solve_part1(puzzle_input) == 5934
 
 
 def test_part2(puzzle_input):
-    assert solve_part2(puzzle_input) is None
+    assert solve_part2(puzzle_input) == 26984457539
