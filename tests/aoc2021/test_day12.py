077682bd82638a8c78e6907bccc5634c18ebc1f1
Simon Keith <simon.keith@shippeo.com>
2021-12-29 00:49:37 +0100

    parametrized tests

--- tests/aoc2021/test_day12.py
+++ tests/aoc2021/test_day12.py
@@ -1,34 +1,63 @@
 from adventofcode.aoc2021.day12 import solve_part1, solve_part2
-from pytest import fixture
+from pytest import fixture, mark
 
 
 @fixture
-def puzzle_input():
-    return [
-        "fs-end",
-        "he-DX",
-        "fs-he",
-        "start-DX",
-        "pj-DX",
-        "end-zg",
-        "zg-sl",
-        "zg-pj",
-        "pj-he",
-        "RW-he",
-        "fs-DX",
-        "pj-RW",
-        "zg-RW",
-        "start-pj",
-        "he-WI",
-        "zg-he",
-        "pj-fs",
-        "start-RW",
-    ]
+def puzzle_input(request):
+    match request.param:
+        case "small":
+            return ["start-A", "start-b", "A-c", "A-b", "b-d", "A-end", "b-end"]
+        case "medium":
+            return [
+                "dc-end",
+                "HN-start",
+                "start-kj",
+                "dc-start",
+                "dc-HN",
+                "LN-dc",
+                "HN-end",
+                "kj-sa",
+                "kj-HN",
+                "kj-dc",
+            ]
+        case "large":
+            return [
+                "fs-end",
+                "he-DX",
+                "fs-he",
+                "start-DX",
+                "pj-DX",
+                "end-zg",
+                "zg-sl",
+                "zg-pj",
+                "pj-he",
+                "RW-he",
+                "fs-DX",
+                "pj-RW",
+                "zg-RW",
+                "start-pj",
+                "he-WI",
+                "zg-he",
+                "pj-fs",
+                "start-RW",
+            ]
+        case _:
+            raise ValueError("invalid fixture parameter")
 
 
-def test_part1(puzzle_input):
-    assert solve_part1(puzzle_input) == 226
+@mark.parametrize(
+    "puzzle_input,expected",
+    [("small", 10), ("medium", 19), ("large", 226)],
+    indirect=["puzzle_input"],
+)
+def test_part1(puzzle_input, expected):
+    assert solve_part1(puzzle_input) == expected
 
 
-def test_part2(puzzle_input):
-    assert solve_part2(puzzle_input) == 3509
+@mark.parametrize(
+    "puzzle_input,expected",
+    [("small", 36), ("medium", 103), ("large", 3509)],
+    indirect=["puzzle_input"],
+)
+def test_part2(puzzle_input, expected):
+    assert solve_part2(puzzle_input) == expected
