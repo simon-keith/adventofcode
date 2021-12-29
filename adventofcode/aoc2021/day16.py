a9c469a556f1ccf711942e0eeb988f1f0bdc563a
Simon Keith <simon.keith@shippeo.com>
2021-12-29 01:52:36 +0100

    day 16

--- adventofcode/aoc2021/day16.py
+++ adventofcode/aoc2021/day16.py
@@ -1,14 +1,125 @@
-from typing import List
+import collections
+import collections.abc
+import math
+from dataclasses import dataclass
+from itertools import islice
+from typing import Iterable, List, Optional, Tuple
 
 from adventofcode.tools.input import read_puzzle_input
 
 
+def parse_puzzle_input(puzzle_input: List[str]) -> str:
+    (transmission,) = puzzle_input
+    return transmission
+
+
+def collect(it: Iterable[str]) -> str:
+    return "".join(it)
+
+
+@dataclass
+class Packet:
+    version: int
+    type_id: int
+    literal: Optional[int] = None
+    sub_packets: Tuple["Packet", ...] = ()
+
+    def __iter__(self):
+        yield self
+        for p in self.sub_packets:
+            yield from p
+
+    def get_value(self) -> int:
+        match self.type_id:
+            case 0:
+                return sum(p.get_value() for p in self.sub_packets)
+            case 1:
+                return math.prod(p.get_value() for p in self.sub_packets)
+            case 2:
+                return min(p.get_value() for p in self.sub_packets)
+            case 3:
+                return max(p.get_value() for p in self.sub_packets)
+            case 4:
+                return self.literal
+            case 5:
+                s1, s2 = self.sub_packets
+                return int(s1.get_value() > s2.get_value())
+            case 6:
+                s1, s2 = self.sub_packets
+                return int(s1.get_value() < s2.get_value())
+            case 7:
+                s1, s2 = self.sub_packets
+                return int(s1.get_value() == s2.get_value())
+            case _:
+                raise ValueError("invalid packet type")
+
+
+class Transmission(collections.abc.Sized, collections.abc.Iterator):
+    def __init__(self, data: str) -> None:
+        num_bits = len(data) * 4
+        binary_data = bin(int(data, 16))[2:].zfill(num_bits)
+        self._i = 0
+        self._n = len(binary_data)
+        self._it = iter(binary_data)
+
+    def __len__(self) -> int:
+        return self._n - self._i
+
+    def __next__(self) -> str:
+        s = next(self._it)
+        self._i += 1
+        return s
+
+    def _fetch(self, size: int) -> str:
+        c = collect(islice(self._it, size))
+        self._i += len(c)
+        return c
+
+    def _get_next_literal(self) -> int:
+        literal = ""
+        while len(self) > 0:
+            group = self._fetch(5)
+            if len(group) != 5:
+                raise ValueError("group is incomplete")
+            literal += "".join(group[1:])
+            if group[0] == "0":
+                return int(literal, 2)
+        raise ValueError("the last group must start with 0")
+
+    def _get_next_header(self) -> Tuple[int, int]:
+        header = tuple(self._fetch(3) for _ in range(2))
+        if not all(len(h) == 3 for h in header):
+            raise ValueError("header is incomplete")
+        return tuple(int(h, 2) for h in header)
+
+    def decode(self) -> Packet:
+        version, type_id = self._get_next_header()
+        if type_id == 4:
+            literal = self._get_next_literal()
+            return Packet(version=version, type_id=type_id, literal=literal)
+        sub_packets = []
+        if next(self) == "0":
+            bits_to_read = int(collect(islice(self, 15)), 2)
+            length_to_reach = len(self) - bits_to_read
+            while length_to_reach < len(self):
+                sub_packets.append(self.decode())
+        else:
+            packets_to_decode = int(collect(islice(self, 11)), 2)
+            for _ in range(packets_to_decode):
+                sub_packets.append(self.decode())
+        return Packet(version=version, type_id=type_id, sub_packets=tuple(sub_packets))
+
+
 def solve_part1(puzzle_input: List[str]) -> int:
-    pass
+    data = parse_puzzle_input(puzzle_input)
+    packet = Transmission(data).decode()
+    return sum(p.version for p in packet)
 
 
 def solve_part2(puzzle_input: List[str]) -> int:
-    pass
+    data = parse_puzzle_input(puzzle_input)
+    packet = Transmission(data).decode()
+    return packet.get_value()
 
 
 if __name__ == "__main__":
--- tests/aoc2021/test_day16.py
+++ tests/aoc2021/test_day16.py
@@ -1,15 +1,60 @@
 from adventofcode.aoc2021.day16 import solve_part1, solve_part2
-from pytest import fixture
+from pytest import fixture, mark
 
 
 @fixture
-def puzzle_input():
-    return []
+def puzzle_input(request):
+    match request.param:
+        case "1a":
+            return ["8A004A801A8002F478"]
+        case "1b":
+            return ["620080001611562C8802118E34"]
+        case "1c":
+            return ["C0015000016115A2E0802F182340"]
+        case "1d":
+            return ["A0016C880162017C3686B18A3D4780"]
+        case "2a":
+            return ["C200B40A82"]
+        case "2b":
+            return ["04005AC33890"]
+        case "2c":
+            return ["880086C3E88112"]
+        case "2d":
+            return ["CE00C43D881120"]
+        case "2e":
+            return ["D8005AC2A8F0"]
+        case "2f":
+            return ["F600BC2D8F"]
+        case "2g":
+            return ["9C005AC2F8F0"]
+        case "2h":
+            return ["9C0141080250320F1802104A08"]
+        case _:
+            raise ValueError("invalid fixture parameter")
 
 
-def test_part1(puzzle_input):
-    assert solve_part1(puzzle_input) is None
+@mark.parametrize(
+    "puzzle_input,expected",
+    [("1a", 16), ("1b", 12), ("1c", 23), ("1d", 31)],
+    indirect=["puzzle_input"],
+)
+def test_part1(puzzle_input, expected):
+    assert solve_part1(puzzle_input) == expected
 
 
-def test_part2(puzzle_input):
-    assert solve_part2(puzzle_input) is None
+@mark.parametrize(
+    "puzzle_input,expected",
+    [
+        ("2a", 3),
+        ("2b", 54),
+        ("2c", 7),
+        ("2d", 9),
+        ("2e", 1),
+        ("2f", 0),
+        ("2g", 0),
+        ("2h", 1),
+    ],
+    indirect=["puzzle_input"],
+)
+def test_part2(puzzle_input, expected):
+    assert solve_part2(puzzle_input) == expected
